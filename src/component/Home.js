869cb9cdb66399b4d4da6919856047dbde03fb1f
biswa418 <biswajeetsutar418@gmail.com>
2023-04-20 01:18:39 +0530

    Home page setup

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,45 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { firestore } from "../firebase";
+import { getDocs, collection } from 'firebase/firestore';
 
 
 function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function getAllPosts() {
+            await getDocs(collection(firestore, 'posts'))
+                .then((snapshot) => {
+                    const newData = snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+                    setPosts(newData);
+                    return newData;
+                });
+        }
+
+        getAllPosts();
+    }, []);
+
+    if (posts.length === 0) return <div>Loading...</div>;
+
     return (
-        <div className="">
-            Home
-        </div>
+        <div className="home">
+            <h1>React Blogs</h1>
+            <div id="blog-by">Biswajeet</div>
+            {
+                posts.map((post, index) => {
+                    return (
+                        <div className="post" key={`post-${index}`}>
+                            <Link to={`post/${post.id}`}>
+                                <h3>{post.title}</h3>
+                            </Link>
+
+                            <p>{post.subTitle}</p>
+                        </div>
+                    )
+                })
+            }
+        </div >
     );
 }
 
